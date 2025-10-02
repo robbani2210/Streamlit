4ee21d53be7294fe28e6d327aba2b1e10b41e575
robbani2210 <91192112+robbani2210@users.noreply.github.com>
2025-10-02 14:15:49 +0700

    Add files via upload

---
+++ dataku.py
@@ -0,0 +1,44 @@
+# import module
+import streamlit as st
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from PIL import Image
+
+
+
+# Title
+st.title("streamlit")
+
+# Header
+st.header("Ayu Belajar Bagaimana Cara Cara Membuat Layouts and Navigation di streamlit")
+
+st.markdown("""
+### 
+
+1. Sidebar
+
+2. Navigation
+
+========================= Contohnya =====================================
+""",True)
+# ============***============
+
+# Cara untuk menampilkan Media elements di streamlit 
+# Sidebar Radio
+
+
+# Sidebar Selectbox
+
+
+# Sidebar Multiselect
+
+
+# Sidebar Input Number
+
+
+# Sidebar Radio Number
+
+
+# Sidebar File Uploader
+
